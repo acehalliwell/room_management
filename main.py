789e5edb4d64e030087229d6bc045d53ab54f516
syamimhilmi <mohamadsyamim.hilmi@sophicautomation.com>
2023-06-23 18:42:17 +0800

    Added CORS

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import sqlite3
 import pandas as pd
 
+
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
